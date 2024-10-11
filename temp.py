56141333525145b9a92ab9349f16e8120401e27c
mārcis <marcis.zebergs@gmail.com>
2024-10-11 12:10:11 +0300

    11.10.2024

--- temp.py
+++ temp.py
@@ -1,20 +1,13 @@
-a = 15
-b = 10
+try:
+    a = int(input("Enter a: "))
+    b =int(input ("Enter b: "))
+    c=int(input ("Enter c: "))
 
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
-print(a ** b)
-print(a // b)
-print(a % b)
-
-a = int (input('Enter number 0:'))
-
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
-print(a ** b)
-print(a // b)
-print(a % b)
+except ValueError:
+    print('a or b or c = int')
+    
+D = b**2-4*a*c
+x1= (-b + D ** 1/2) / (2 * a)
+x2= (-b - D ** 1/2) / (2 * a)
+print(x1)
+print(x2)
