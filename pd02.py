1a0ae58f35011e684653e142ff6a59206b398dae
ViktorijaKergalve <5331vika@gmail.com>
2024-12-13 12:28:48 +0200

    Add files via upload

---
+++ pd02.py
@@ -0,0 +1,37 @@
+# 1.uzdevums
+import random
+n = int(input(" ievadi skaitli:"))
+summa = 0
+for i in range(1, 1 + n):
+    summa += i
+print("skaitļu summa no 1 lidz n ir:", summa)
+# 2.uzdevums
+n = int(input(" ievadi skaitli:"))
+faktorials = 1
+for i in range(1, 1 + n):
+    faktorials *= i
+print("skaitļu faktorials no 1 lidz n ir:", faktorials)
+# 3.uzdevums
+n = int(input(" ievadi skaitli:"))
+for i in range(n, -1, -1):
+        print(i)
+# 4.uzdevums
+length = int(input("h: "))
+for i in range(length, 0, -1):
+    print("* " * i)
+# 5.uzdevums
+import random
+min_skaitlis = 0
+max_skaitlis = 100
+izveles_skaitlis = random.randint(min_skaitlis, max_skaitlis)
+print("uzminet skaitli no 0 lidz 100")
+uzminets = False
+while not uzminets:
+    skaitlis = int(input("ievadiet skaitli:"))
+    if skaitlis < izveles_skaitlis:
+            print("Lielaks!")
+    if skaitlis < izveles_skaitlis:
+        print("Mazāks!")
+    if skaitlis < izveles_skaitlis:
+        print("Uzminēts!")
+        break
